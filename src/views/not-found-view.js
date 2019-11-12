c310460045e14fc1dc074725a3bdf635e8a5f177
Tony Medrano <info@tonymedrano.com>
2019-11-12 11:20:11 +0100

    Adding updated files

---
+++ src/views/not-found-view.js
@@ -0,0 +1,12 @@
+import { html } from 'lit-element';
+import { BaseView } from './base-view';
+
+class NotFoundView extends BaseView {
+  render() {
+    return html`
+      <h1>Not found!</h1>
+    `;
+  }
+}
+
+customElements.define('not-found-view', NotFoundView);
